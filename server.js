1cbcdc6b453570bce6cea444ee029c767c8e1ee3
Lutfi <lutfifitriainsani@gmail.com>
2018-10-18 12:03:31 +0700

    deploy github

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const restify = require('restify');
+global.config = require('./config/app');
+
+const server = restify.createServer({
+    name : 'Githhub Deploy',
+    version : '1.0.0'
+});
+
+server.get('/', restify.plugins.serveStatic({
+    directory : __dirname,
+    default : '/index.html'
+}));
+
+server.listen(config.port, function(){
+    console.log('% listen at s%', server.name, server.url)
+});
